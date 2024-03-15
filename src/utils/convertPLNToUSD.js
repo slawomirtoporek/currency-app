83aa813521bbfcb28ff3ed4b430c1cfedf42bf61
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-03-15 21:00:35 +0100

    add test to check the convertPLNToUSD function wheter its argument is text

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,14 @@
 export const convertPLNToUSD = (PLN) => {
+  
+  if(typeof PLN === 'string'){
+    return NaN;
+  };
+
 
   const PLNtoUSD = PLN / 3.5;
-  
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
   });
-
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
 }
